57d7cb217f586ffc4a173dbc5e03b5c704398985
dsenat <dayanasenat@gmail.com>
2023-03-07 09:51:16 -0500

    added variable-hw

--- 0-data-types/assignments/variable-hw.js
+++ 0-data-types/assignments/variable-hw.js
@@ -1,9 +1,9 @@
-// TEST COMMENT// Create a .js file that satisfies assignment below 
+// Create a .js file that satisfies assignment below 
 // Copy the final assignment text into this assignment to submit
 // For each exercise you'll want to log the result to the console to check your work
 
 
-//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least variables.
 
 
 //2. Create a set of variables that define a vehicle. Must be at least 6 variables.
