2a936f3a0fd712fe14cbb7f169943241af4273cf
dsenat <dayanasenat@gmail.com>
2023-03-10 00:26:16 -0500

    strings-hw

---
+++ 3-strings/assignments/strings-hw.js
@@ -0,0 +1,59 @@
+
+// 1. Concatenate "hello" with "goodbye"
+const string1 = "hello"
+const string2  = "goodbye"
+console.log(string1 + string2)
+// 2. Save the substring "Friend" from the string "Hello Friend"
+const greeting = "Hello Friend"
+console.log(greeting.slice(6,12))
+// 3. Using 5 or more of the following words, create a template literal that makes a legible sentence.
+//    Do not create your own words, use the existing ones below!
+const word1 = "hello"; 
+const word2 = "horror";
+const word3 = "peace";
+const word4 = "world"
+const word5 = "dislike"; 
+const word6 = "umbrella"; 
+const word7 = "car"; 
+const word8 = "pasta"; 
+const word9 = "pizza"; 
+const word10 = "movies";
+const word11 = "football"; 
+const word12 = "soccer"; 
+const word13 = "eat"; 
+const word14 = "cookie"; 
+const word15 = "favorite";
+const word16 = "makeup"; 
+const word17 = "I"; 
+const word18 = "to"; 
+const word19 = "my";
+const word20 = "is";
+const word21 = "and";
+const word22 = "also";
+const word23 = "like"; 
+I `${word23}``${word16}`alot `${word17}``${word22}``${word23}``${peace}`
+// 4. Given the string of your full name get your initials.
+let fullName = "Dayana Senat"
+let initials = fullName[0] + fullName [7]
+// 5. Get the 5th character from the following string: 'Hello World'
+const greeting1 = 'Hello World'
+console.log(greeting1 =[""])
+// 6. Find the index of the dash from 'Coding-Time!'
+const coding = 'Coding-Time!'
+console.log(coding.indexOf("-"))
+// 7. Write code to grab the final letter from a any string.
+const string = "string"
+const lastLetter = string[string.lenghth-1]
+// 8. Write code to grab the first 3 letters from a string. 
+const letters = "string"
+console.log(letters[0])
+console.log(letters[1])
+console.log(letters[2])
+// 9. Turn the following string into an array of words: 'This-is-a-sentence';
+const sentence1 = 'This-is-a-sentence'
+console.log(sentence1.split(""))
+// 10. Make the string of 12 from the following two numbers:
+let one = 1;
+let two = 2;
+(1).toString()
+(2).toString()
