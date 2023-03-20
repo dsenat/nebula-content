9666b0c98a13f03256962a3cce03874a1443cc1f
dsenat <dayanasenat@gmail.com>
2023-03-20 03:10:58 -0400

    function hw

---
+++ 7-functions/assignments/function-hw.js
@@ -0,0 +1,41 @@
+// 1. Write a function that takes in an array of numbers and returns a new array containing the numbers with one added to each
+// Example you pass in the following array [1, 2, 3] the function should return [2, 3, 4]
+function numbers ([20,60,44]){
+    let newNumbers = []
+    for(let i = 0; i < numbers.length; i++)
+    newNumbers.push(numbers[i])
+
+}
+  
+  // 2. Write a function that takes in a string, and a letter, and returns true or false if the string contains the letter.
+  // Example string = "happy birthday", letter = "a", should return true
+  // Example string = "happy birthday", letter = "q" should return false
+function words ("going for a walk", "D"){
+    for(let i = 0; i < words.length; i++){
+     if(words[i] === letter)   
+    }
+}
+  
+  // 3. Write a function that takes in an array of full names (formatted "firstName lastName"), and returns a new array containing only first names.
+  // Example array = ["Tom Smith", "Bob Jones", "Bill Williams"] returns ["Tom", "Bob", "Bill"]
+  function fullNames ["Bryson Tiller", "Brent Faiyaz", "Summer Walker", "Jhene Aiko","Chris Brown"](){
+    let firstNames = []
+    for(let i = 0; i < fullNames.length; i++){
+    let firstNames = fullNames.split(" ");
+    return firstNames
+  }
+  }
+
+  
+  // 4. Write a function that takes in a name and a greeting and logs a string greeting the person to the console.
+  // Example name = "John Boy" greeting = Goodnight, returns "Goodnight John Boy"
+  function greet (name1, greeting){
+  console.log(`${greeting}, ${name1}`)
+  }
+greet("Goodnight","Dayana")
+  
+  // 5. Write a function that takes in the year someone was born and returns their age.
+  // Example: birth year: 2000 returns 23
+  function Age (){
+    let year = 1991
+  }
