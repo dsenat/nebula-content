07fe80ab2f003ba3fd696b9c9e554bad44f2a7a3
dsenat <dayanasenat@gmail.com>
2023-03-30 00:22:57 -0400

    added domhw

---
+++ 11-dom-manipulation/practice/domprac.js
@@ -0,0 +1,19 @@
+// Grab the elements and log them to the console
+
+// Q1:  Attach this document to the index.html
+
+
+// Q2:  Grab the second element that has the text Hello
+const h1 = document.querySelector("h1")
+
+// Q3:  Grab the text in the h1 that says 'Good Afternoon'
+const h1s = document.querySelectorAll("h1");
+
+console.log(h1s)
+console.log(h1s[2].innertext)
+
+// Q4:  Grab the h1 with the text that says "Catch me if you can"
+const catchMe = document.querySelectorAll(".catch-me")
+
+// Q5:  Grab the span element and change the text to "Got ya"
+const span = document.querySelectorAll()
